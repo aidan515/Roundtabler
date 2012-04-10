0f3457c34550d16d444d31a7a8995cf57909a19b
Aidan Corbett <aidan515@gmail.com>
2012-04-10 12:52:25 +0100

    Twitter bootstrap installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roundtabler::Application.config.secret_token = 'ef41b19e06939cbacd72ed603c30cf24a4c9c58cb5d37f5a47b4a6be7580be79d525287e5625c35a05df78f396f099b5e4c552f9cd2c112a3d022306b2e635ca'
